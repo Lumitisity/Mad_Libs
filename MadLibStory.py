2ab4a8696f99120ce90aad3532eb7880fd01d865
Lumitisity <pokekatcher1348@gmail.com>
2026-07-01 13:41:11 -0700

    Add files via upload

---
+++ MadLibStory.py
@@ -0,0 +1,16 @@
+loc1 = input("Enter a place: ")
+noun1 = input("Enter a animal: ")
+verb1 = input("Enter a verb: ")
+adverb1 = input("Enter an adverb: ")
+noun2 = input("Enter a noun: ")
+verb2 = input("Enter a verb: ")
+adverb2 = input("Enter an adverb: ")
+em1 = input("Enter an emotion: ")
+noun3 = input("Enter a noun: ")
+ad1 = input("Enter an adjective: ")
+noun4 = input("Enter a noun: ")
+
+print(f"""
+Today I went to the {loc1}. I saw a(n) {noun1} {verb1} {adverb1}. 
+Then I saw a {noun2} and I {verb2} {adverb2}.
+I was {em1} about seeing the {noun3}.  What a {ad1} {noun4}!""")
